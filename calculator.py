f489a45739e6dce83252a813b941c40b50c960ae
Roger González <rogergonzalez21@gmail.com>
2017-09-04 09:24:11 -0400

    Code for second python challenge

--- calculator.py
+++ calculator.py
@@ -1,16 +1,28 @@
-"""
-Your module documentation here
+"""Vauxoo Challenge "calculator" challenge
+
+This module hosts the class and functions that calculates if any given number
+is a prime number.
 """
 
 
 class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
+    """This class hosts the function that sums all numbers of any given list.
 
-    def sum(self, num_list):
+    """
+    def sum(num_list):
+        """Service that calculates the sum of a number list
+
+        Args:
+            num_list (list): A list containing all the numbers to sum
+
+        Returns:
+            int: Total sum of all the numbers in the list
         """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+
+        total = 0
+        for number in num_list:
+            try:
+                total += number
+            except TypeError:
+                pass
+        return total
